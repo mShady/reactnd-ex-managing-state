203c51d532bc5002d7811e607d6912bfbf422c00
Shady M. Najib <smostafa@integrant.com>
2019-04-06 22:23:14 +0200

    Wiring Game component in App

--- src/App.js
+++ src/App.js
@@ -1,15 +1,40 @@
 import React, { Component } from "react";
 import logo from "./logo.svg";
 import "./App.css";
-
-const value1 = Math.floor(Math.random() * 100);
-const value2 = Math.floor(Math.random() * 100);
-const value3 = Math.floor(Math.random() * 100);
-const proposedAnswer = Math.floor(Math.random() * 3) + value1 + value2 + value3;
-const numQuestions = 0;
-const numCorrect = 0;
+import Game from "./Game";
 
 class App extends Component {
+  state = { ...this.getNewEquation(), numQuestions: 0, numCorrect: 0 };
+
+  getNewEquation() {
+    let value1 = Math.floor(Math.random() * 100);
+    let value2 = Math.floor(Math.random() * 100);
+    let value3 = Math.floor(Math.random() * 100);
+    let proposedAnswer =
+      Math.floor(Math.random() * 3) + value1 + value2 + value3;
+    return {
+      value1,
+      value2,
+      value3,
+      proposedAnswer
+    };
+  }
+
+  gradeAnswer(Answer) {
+    let { value1, value2, value3, proposedAnswer } = this.state;
+
+    let isAnswerCorrect =
+      (value1 + value2 + value3 === proposedAnswer) === Answer;
+
+    this.setState(currentState => ({
+      ...this.getNewEquation(),
+      numQuestions: currentState.numQuestions + 1,
+      numCorrect: isAnswerCorrect
+        ? currentState.numCorrect + 1
+        : currentState.numCorrect
+    }));
+  }
+
   render() {
     return (
       <div className="App">
@@ -17,17 +42,29 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">ReactND - Coding Practice</h1>
         </header>
-        <div className="game">
+        <Game
+          value1={this.state.value1}
+          value2={this.state.value2}
+          value3={this.state.value3}
+          proposedAnswer={this.state.proposedAnswer}
+          onGradeAnswer={Answer => this.gradeAnswer(Answer)}
+          numCorrect={this.state.numCorrect}
+          numQuestions={this.state.numQuestions}
+        />
+        {/* <div className="game">
           <h2>Mental Math</h2>
           <div className="equation">
-            <p className="text">{`${value1} + ${value2} + ${value3} = ${proposedAnswer}`}</p>
+            <p className="text">{`${this.state.value1} + ${
+              this.state.value2
+            } + ${this.state.value3} = ${this.state.proposedAnswer}`}</p>
           </div>
-          <button>True</button>
-          <button>False</button>
-          <p className="text">
-            Your Score: {numCorrect}/{numQuestions}
-          </p>
-        </div>
+          <button onClick={() => this.gradeAnswer(true)}>True</button>
+          <button onClick={() => this.gradeAnswer(false)}>False</button>
+          <Score
+            numCorrect={this.state.numCorrect}
+            numQuestions={this.state.numQuestions}
+          />
+        </div> */}
       </div>
     );
   }
