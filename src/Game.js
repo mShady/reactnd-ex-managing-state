6f243835271d5d9237bd0a985349d5c845d4a686
Shady M. Najib <smostafa@integrant.com>
2019-04-06 22:22:58 +0200

    Adding Game Component

---
+++ src/Game.js
@@ -0,0 +1,20 @@
+import React from "react";
+import Score from "./Score";
+
+function Game(props) {
+  return (
+    <div className="game">
+      <h2>Mental Math</h2>
+      <div className="equation">
+        <p className="text">{`${props.value1} + ${props.value2} + ${
+          props.value3
+        } = ${props.proposedAnswer}`}</p>
+      </div>
+      <button onClick={() => props.onGradeAnswer(true)}>True</button>
+      <button onClick={() => props.onGradeAnswer(false)}>False</button>
+      <Score numCorrect={props.numCorrect} numQuestions={props.numQuestions} />
+    </div>
+  );
+}
+
+export default Game;
